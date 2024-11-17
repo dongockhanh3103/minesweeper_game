63d6c24c286ca24f092de222c6b4670b9ae1633f
Khanh Do <khanh.do@oivan.com>
2024-11-17 16:51:06 +0700

    feature: update game helper

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -2,13 +2,13 @@ module GameHelper
   def status_label(status)
     case status
     when Game.statuses[:playing]
-      content_tag(:span, I18n.t("games.statuses.playing"), class: 'badge bg-warning')
+      content_tag(:span, I18n.t('games.statuses.playing'), class: 'badge bg-warning')
     when Game.statuses[:lost]
-      content_tag(:span, I18n.t("games.statuses.lost"), class: 'badge bg-danger')
+      content_tag(:span, I18n.t('games.statuses.lost'), class: 'badge bg-danger')
     when Game.statuses[:won]
-      content_tag(:span, I18n.t("games.statuses.won"), class: 'badge bg-success')
+      content_tag(:span, I18n.t('games.statuses.won'), class: 'badge bg-success')
     when Game.statuses[:initial]
-      content_tag(:span, I18n.t("games.statuses.won"), class: 'badge bg-secondary')
+      content_tag(:span, I18n.t('games.statuses.initial'), class: 'badge bg-secondary')
     end
   end
 
