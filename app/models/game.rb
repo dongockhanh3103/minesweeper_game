0fd242f07a66a3e48de01c5c01f47f8d5d6ac1df
Khanh Do <khanh.do@oivan.com>
2024-11-18 10:03:16 +0700

    feature: validate number_of_mines

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,10 +4,17 @@ class Game < ApplicationRecord
   validates :name, :email, :width, :height, :number_of_mines, presence: true
   validates :email, format: URI::MailTo::EMAIL_REGEXP
   validates :width, :height, :number_of_mines, numericality: { other_than: 0 }
+  validate :validate_number_of_mines
 
   after_create :initialize_board
 
   def initialize_board
     Games::InitializeBoard.call(self)
+  end
+
+  private
+
+  def validate_number_of_mines
+    errors.add(:number_of_mines, "must be less than #{width * height}") if number_of_mines > width * height
   end
 end
